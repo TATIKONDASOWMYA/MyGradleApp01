7dd8625dae7ef2c756feb88fafee75aefba176a2
TATIKONDASOWMYA <tatikondasowmya2005@gmail.com>
2026-06-16 12:41:02 +0530

    First Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,11 +3,9 @@
  */
 public class App {
     public String getGreeting() {
-<<<<<<< HEAD
-        return "Hello world.";
-=======
+
         return "Sowmya A \n1BI23CS211";
->>>>>>> d110f83 (FIRST COMMIT)
+
     }
 
     public static void main(String[] args) {
